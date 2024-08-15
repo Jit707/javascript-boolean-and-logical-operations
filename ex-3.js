c8e9af0c5f7098da97bad9918526d6105b91a63f
Jit <jit.oung254343@gmail.com>
2024-08-15 21:10:01 +0700

    add finished assignment

--- ex-3.js
+++ ex-3.js
@@ -1,9 +1,12 @@
 //Exercise #3: Debugging (Boolean & Logical Operations)
 
 //Start coding here
-const numberA = 100;
+let numberA = 100;
 const numberB = "20";
+typeof numberB
+console.log(typeof numberB);
+
 
 numberA = 10;
 
-console.log(`Sum: ${numberA + numberB}`);
+console.log(`Sum: ${numberA + Number(numberB)}`);
